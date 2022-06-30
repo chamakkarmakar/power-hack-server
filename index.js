72bd625e6b6af73b903030ef482cbe755bb992aa
chamakkarmakar <chamakkarmakar.ck@gmail.com>
2022-06-30 17:56:05 +0600

    database connected successfully

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.zkum8.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const billingCollection = client.db('dbBilling_info').collection('biling');
+        console.log('Billing database connected');
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
 
 // Root API
 app.get('/', (req, res) => {
